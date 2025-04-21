6962f591e82a980f0f4e76e5d457b809266ff416
Liu Xinyu <liuxinyu99@hotmail.com>
2025-04-21 14:12:10 +0800

    added all answers to ch03 except for the best decompose of Egyptian fraction

--- fraction/src/Inherit.hs
+++ fraction/src/Inherit.hs
@@ -1,7 +1,6 @@
 module Inherit where
 
 s = [(a, b, c, n) | a <- [2..10], b <- [a+1..11], c <- [b+1..12],
-     let d = a*b*c - a*b - b*c - a*c,
-         d > 0,
-     let n = a*b*c `div` d,
-         n > 1, n `mod` a == 0, n `mod` b == 0, n `mod` c == 0]
+     let d = a*b*c - a*b - b*c - a*c, d > 0,
+     let n = a*b*c `div` d, n > 1,
+         n `mod` a == 0, n `mod` b == 0, n `mod` c == 0]
